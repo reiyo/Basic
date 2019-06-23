227a4a7614f253c7bb4af638187cccbf394543b1
reiyo <khho@eda.ee.ntu.edu.tw>
2019-06-24 01:41:40 +0800

    Add deque with obj.

--- Memo_Python/deque.py
+++ Memo_Python/deque.py
@@ -13,6 +13,8 @@ from collections import deque
 # pop():        pop data from the right side
 # popleft():    pop data from the left side
 
+##########################
+
 d1 = deque()
 d1.extend('abc')
 d1.append('d')
@@ -22,6 +24,7 @@ while len(d1) != 0:
     print ("d1[0] = %s , d1[-1] = %s" % (d1[0], d1[-1]))
     d1.pop()
 
+##########################
 
 d2 = deque()
 d2.extendleft('def')
@@ -31,4 +34,19 @@ print (d2) # deque(['g', 'f', 'e', 'd'])
 while len(d2) != 0:
     print ("d2[0] = %s , d2[-1] = %s" % (d2[0], d2[-1]))
     d2.popleft()
+
+##########################
+
+class MyObject(object):
+    def __init__(self, val):
+        self.val = val
+
+d3 = deque()
+d3.append(MyObject(40))
+d3.append(MyObject(70))
+d3.append(MyObject(30))
+
+while len(d3) != 0:
+    print ("d3[0] = %d , d3[-1] = %d" % (d3[0].val, d3[-1].val))
+    d3.pop()
 
